622042d04c97d79a5788b7c13b7c693d5324f111
abhishekbhartiind <imabhishekbharti12@gmail.com>
2023-03-24 00:43:09 +0530

    message scroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import TopCommentBox from "./components/CommentBox/TopCommentBox"
+import MessageScroll from "./components/MessageScroll"
 
 function App() {
   return (
     <div className="App">
       <TopCommentBox autoFocus={false} />
+      <MessageScroll />
     </div>
   )
 }
